047182a8b1497d51efc6f3d58db3a57a98c3729d
Alteri <trochonowicz.jacek@gmail.com>
2022-03-12 10:02:28 +0100

    add colors and globalstyle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { Provider } from "react-redux";
 import type { AppProps } from "next/app";
+import { GlobalStyle } from "../components/Global/globalStyle";
 import { store } from "../store";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
+      <GlobalStyle />
       <Component {...pageProps} />
     </Provider>
   );
-}
+};
 
 export default MyApp;
