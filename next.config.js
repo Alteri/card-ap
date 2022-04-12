f8f30a4b9f9f25a81b7bec06a2dbf04faa9536fc
Alteri <trochonowicz.jacek@gmail.com>
2022-04-12 08:24:59 +0200

    condition basePath

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  basePath: "/card-ap",
+  basePath: isProd ? "/card-ap" : "",
   assetPrefix: isProd ? "/card-ap/" : "",
 };
